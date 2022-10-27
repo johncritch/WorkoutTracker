5743bf5c9a3b669e7e9fb87aaf4044598accff4f
John Critchlow <80559126+johncritch@users.noreply.github.com>
2022-10-26 20:39:49 -0600

    Initial Commit

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by John Critchlow on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
